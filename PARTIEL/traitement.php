5d8e955ecb58517be19c923115b2a30d514d4144
Victor <victorgms0307@gmail.com>
2025-02-05 09:18:40 +0100

    Modification Luca

--- PARTIEL/traitement.php
+++ PARTIEL/traitement.php
@@ -1,14 +1,14 @@
 <?php
 
 
-$developpeur = array(new Developpeur(550,"Le Flock",
-"Louis-Marie", "lmleflock@test.net","0606060606",2500,
-"Java"),
-new Developpeur(650,"Poirie", "Ethan", "ach@test.net",
-"0707070707",2750, "C++")) ; ;
-
-$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net", "0606060606",4000, "DSI"), new Manager(600,"OLIVEIRA", "Romain", "romain@manager.net", "0808080808",3000, "Chef de service")) ;
-
+$developpeur = array( new Developpeur(550, "Le Flock", "Louis-Marie",
+"lmleflock@test.net", "0606060606", 2500, "Java"),
+new Developpeur(650, "Chellal", "Ali", "ach@test.net", "0707070707", 2750,
+"C++"),
+new Developpeur(151,"Katsiev", "Amir", "amir@test.net", "0606060606",2750,
+"Kotlin")) ;
+$manager = array(new Manager(500, "RIPAULT", "Alexis", "alexis@mana.net",
+"0606060606",4000, "DSI")) ;
 
 foreach ($developpeur as $value) {
 	echo $value->afficher();
@@ -18,4 +18,7 @@ echo "<br/><br/><br/>";
 foreach ($manager as $value) {
 	echo $value->afficher();
 }
+
+
+
 ?>
